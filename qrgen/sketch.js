fcf879a91124e07dc14a883c4d360c4e6d296c45
Satoshi Soma <noreply@amekusa.com>
2026-05-12 14:27:31 +0900

    保存ボタン追加

--- qrgen/sketch.js
+++ qrgen/sketch.js
@@ -5,6 +5,7 @@ let url;
 let input; // 入力欄
 let colorInput;
 let bgInput;
+let button;
 
 function setup() {
 	createCanvas(400, 400);
@@ -12,6 +13,8 @@ function setup() {
 	input = select('#url'); // HTMLタグを取得する（id は url）
 	colorInput = select('#color');
 	bgInput = select('#bg');
+  button = select('#save'); // 保存ボタンを取得
+  button.mousePressed(saveQR); // （予約）ボタンを押すと saveQR を呼ぶ
 }
 
 function draw() {
@@ -29,6 +32,11 @@ function draw() {
 	noStroke();
 	fill(colorInput.value()); // 塗りの色
 
+  let w = gap * qrData.length; // 幅の px
+  let h = w;                   // 高さの px
+
+  // キャンバスの基準位置をずらす
+  translate(200 - w/2 + gap/2, 200 - h/2 + gap/2);
 	for (let y = 0; y < qrData.length; y++) {
 		let line = qrData[y]; // y 番目の列
 		for (let x = 0; x < line.length; x++) {
@@ -42,3 +50,7 @@ function draw() {
 	}
 }
 
+// QR コード画像として保存する
+function saveQR() {
+  saveCanvas('qr.png');
+}
